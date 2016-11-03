04f01ae8cde93c94c951375aa0bd5ad2195bbfc7
JeanMichelSimplon <jean.michel.simplon@gmail.com>
2016-11-03 13:16:00 +0100

    Commit FizzBuzz via Eclips

---
+++ src/co/simplon/prairie/Main.java
@@ -0,0 +1,15 @@
+package co.simplon.prairie;
+public class Main {
+
+	public static void main(String args[]) {
+		/*
+            TODO :
+
+            Ecrire le code permettant l'affichage attendu pour la plage [0 ; 5]
+            en utilisant la classe FizzBuzz */
+
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		fizzbuzz.afficherListeEntiers(0, 50);
+	}
+
+}
